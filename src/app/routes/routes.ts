782754f3e1b44be23b5a0e155f9cdf20e8306762
fha96 <fahadalrimawe96@hotmail.com>
2023-06-16 17:23:12 +0300

    feat(app): setting up routes

---
+++ src/app/routes/routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "../home/home.component";
+import { ServersComponent } from "../servers/servers.component";
+import { UsersComponent } from "../users/users.component";
+
+
+export const routes: Routes = [
+    {
+        path:'', component:HomeComponent
+    },
+    {
+        path:'users', component: UsersComponent
+    },
+    {
+        path:'servers', component: ServersComponent
+    },
+] 
